f21b7caffa349df987c5dd9784ec36df5f8a1ab6
atmta <atmta@mail.com>
2015-06-11 14:32:11 +0300

    add admin and feed

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
     $("#achivment_achive_type").change(function () {
         if (this.value == "run"||this.value == "walk") {
+            $('#achivment_count').hide();
+            $('.achivment_content_info').show();
+            $('.achivment_start_info').show();
+            $('.achivment_finish_info').show();
             $('#achivment_content').show();
             $('#achivment_start').show();
             $('#achivment_finish').show();
             $('#achivment_time').show();
-            $('#achivment_count').show();
         } else if (this.value == "medium") {
+            $('.achivment_content_info').show();
             $('#achivment_content').show();
             $('#achivment_count').show();
+            $('.achivment_start_info').hide();
+            $('.achivment_finish_info').hide();
+            $('#achivment_start').hide();
+            $('#achivment_finish').hide();
+            $('#achivment_time').hide();
+
         } else {
+            $('.achivment_content_info').hide();
+            $('.achivment_start_info').hide();
+            $('.achivment_finish_info').hide();
             $('#achivment_content').hide();
             $('#achivment_start').hide();
             $('#achivment_finish').hide();
